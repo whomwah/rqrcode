afec8508eedee11cc4754cdc1cc4072ae9dadafb
whomwah <whomwah@70f6ffcc-3070-48c7-8e95-2772f12856e8>
2008-02-23 13:12:18 +0000

    added some license stuff remove pureimage file from source
    git-svn-id: svn+ssh://rubyforge/var/svn/rqrcode/trunk@17 70f6ffcc-3070-48c7-8e95-2772f12856e8

--- lib/rqrcode/qrcode.rb
+++ lib/rqrcode/qrcode.rb
@@ -1,3 +1,14 @@
+#!/usr/bin/env ruby
+
+#--
+# Copyright 2004 by Duncan Robertson (duncan@whomwah.com).
+# All rights reserved.
+
+# Permission is granted for use, copying, modification, distribution,
+# and distribution of modified versions of this work as long as the
+# above copyright notice is included.
+#++
+
 module RQRCode
 
   QRMODE = {
