d85e61dccb6dcd356cfce0c1175cc3bf7fcf2140
whomwah <whomwah@70f6ffcc-3070-48c7-8e95-2772f12856e8>
2008-02-24 11:33:19 +0000

    update tests update to version 0.2.1
    git-svn-id: svn+ssh://rubyforge/var/svn/rqrcode/trunk@33 70f6ffcc-3070-48c7-8e95-2772f12856e8

--- test/runtest.rb
+++ test/runtest.rb
@@ -19,8 +19,7 @@ class QRCodeTest < Test::Unit::TestCase
   def test_H_
 		qr = RQRCode::QRCode.new( 'duncan', :size => 1 )
 
-    assert_equal qr.modules.length, 21
-    assert_equal qr.module_count, 21
+    assert_equal qr.modules.size, 21
 		assert_equal qr.modules, MATRIX_1_H
 
 		qr = RQRCode::QRCode.new( 'duncan', :size => 1 )
@@ -36,24 +35,21 @@ class QRCodeTest < Test::Unit::TestCase
   def test_3_H_
 		qr = RQRCode::QRCode.new( 'duncan', :size => 3 )
 
-    assert_equal qr.modules.length, 29
-    assert_equal qr.module_count, 29
+    assert_equal qr.modules.size, 29
 		assert_equal qr.modules, MATRIX_3_H
   end
 
   def test_5_H_
 		qr = RQRCode::QRCode.new( 'duncan', :size => 5 )
 
-    assert_equal qr.modules.length, 37
-    assert_equal qr.module_count, 37
+    assert_equal qr.modules.size, 37
 		assert_equal qr.modules, MATRIX_5_H
   end
 
   def test_10_H_
 		qr = RQRCode::QRCode.new( 'duncan', :size => 10 )
 
-    assert_equal qr.modules.length, 57
-    assert_equal qr.module_count, 57
+    assert_equal qr.modules.size, 57
 		assert_equal qr.modules, MATRIX_10_H
   end
 
@@ -69,8 +65,7 @@ class QRCodeTest < Test::Unit::TestCase
 		assert_equal qr.modules, MATRIX_4_Q
 
 		qr = RQRCode::QRCode.new('www.bbc.co.uk/programmes/b0090blw')
-    assert_equal qr.modules.length, 33
-    assert_equal qr.module_count, 33
+    assert_equal qr.modules.size, 33
 		assert_equal qr.modules, MATRIX_4_H
   end
 
