7091b636dd48d8c63ccd3de69ccdcca3d1a1228c
Christopher Lord <christopher@lord.ac>
2015-08-12 08:31:23 -0600

    Add crispEdges attribute to disable antialiasing
    Antialiasing on SVG can affect QR success rate.

--- lib/rqrcode/export/svg.rb
+++ lib/rqrcode/export/svg.rb
@@ -22,7 +22,7 @@ module RQRCode
         dimension = (self.module_count*module_size) + (2*offset)
 
         xml_tag = %{<?xml version="1.0" standalone="yes"?>}
-        open_tag = %{<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events" width="#{dimension}" height="#{dimension}">}
+        open_tag = %{<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events" width="#{dimension}" height="#{dimension}" shape-rendering="crispEdges">}
         close_tag = "</svg>"
 
         result = []
